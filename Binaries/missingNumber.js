0d6c287ab715ca835bead92c0ae2f1e56b49c3a7
stuartcaio <caiostuart06@gmail.com>
2025-05-20 22:45:42 +0100

    Finding the missed number using xor operator.

---
+++ Binaries/missingNumber.js
@@ -0,0 +1,21 @@
+function missingNumber(numbers = []){
+    let missedNumber = 0;
+
+    const firstArray = [];
+    const secondArray = [];
+
+    for(let x = 0; x < numbers.length; x++){
+        missedNumber ^= numbers[x];
+        firstArray.push(numbers[x]);
+    };
+
+    for(let y = 0; y <= numbers[numbers.length - 1]; y++){
+        missedNumber ^= y;
+        secondArray.push(y);
+    };
+    return missedNumber;
+};
+
+console.log(missingNumber([0,1,2,4]));
+console.log(missingNumber([0,1,2,3,5]));
+console.log(missingNumber([0,1,2,3,4,5,7,8]));
